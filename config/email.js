bb8dddbac5b21e379b8746bfcba627add87714ad
yizhe <liaoyizhe75@gmail.com>
2025-02-20 12:44:52 +0800

    Fix wrong env name

--- config/email.js
+++ config/email.js
@@ -4,8 +4,8 @@ module.exports = {
     emailConfig: {
         service: 'gmail',
         auth: {
-            user: process.env.Email_User,
-            pass: process.env.Email_Password,
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
         }
     }
 };
